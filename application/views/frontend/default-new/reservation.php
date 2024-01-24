4b1c38568a2d79f173d84b02b513f77a03db95b8
souheibmadi <sm@aimngt.com>
2024-01-24 17:54:04 +0100

    fix stats ERTER E

--- application/views/frontend/default-new/reservation.php
+++ application/views/frontend/default-new/reservation.php
@@ -14,8 +14,10 @@
         	<img style="display:block; margin-left: auto; margin-right: auto;margin-top:10px;" src="<?php  echo base_url('assets/frontend/default-new/img/reservation.png') ; ?>">
         	                   <h1 class="text-center" style="padding-top:5%;"> حجز دورة عملية  </h1>
                         <p class="mb-3"> </p>
-<div class="calendly-inline-widget" data-url="https://calendly.com/yakoubmadi/business" style="min-width:320px;height:700px;"></div>
+<!-- Calendly inline widget begin -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/abdullah-alharbi4fm/practical?primary_color=0753bd" style="min-width:320px;height:700px;"></div>
 <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Calendly inline widget end -->
 
 </div>
 </div>
